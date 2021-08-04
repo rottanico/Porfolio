27ba8e552c2810e5f28351dcc129192d6bf8013a
rottanico <rottanico@gmail.com>
2021-08-04 07:29:52 -0300

    first commit

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+
+
+const Navbar = function(){
+
+
+
+    return (<div>
+        <span>desarrollador</span>
+        </div>)
+}
+export default Navbar;
